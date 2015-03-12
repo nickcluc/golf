f5681566db0ec59c9cc735b6a3c4ae6eeffa6ed0
nickcluc <nclucas01@gmail.com>
2015-03-12 11:36:24 -0400

    bugfixes

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,8 @@
 class Friendship < ActiveRecord::Base
   belongs_to :user
   belongs_to :friend, class_name: "User"
+
+  def self.accepted
+    where(accepted: true)
+  end
 end
--- spec/features/user_adds_friend_spec.rb
+++ spec/features/user_adds_friend_spec.rb
@@ -12,25 +12,27 @@ feature "User adds friends", %{
   [ ] Friends can be added to rounds that you have planned or have played.
 } do
 
+  before :each do
+    @test_user_one = FactoryGirl.create(:user)
+    @test_user_one.reindex
+    User.searchkick_index.refresh
+    @test_user_two = FactoryGirl.create(:user, first_name: "Michael")
+    @test_user_two.reindex
+    User.searchkick_index.refresh
+  end
+
   scenario "two users successfully become friends" do
-    test_user_one = FactoryGirl.create(:user)
-    test_user_one.reindex
-    User.searchkick_index.refresh
-    test_user_two = FactoryGirl.create(:user, first_name: "Michael")
-    test_user_two.reindex
-    User.searchkick_index.refresh
+    sign_in_as(@test_user_one)
 
-    sign_in_as(test_user_one)
-
-    visit user_path(test_user_two)
+    visit user_path(@test_user_two)
 
     click_link "Add Friend"
 
-    expect(page).to have_content "Friend Request to #{test_user_two.full_name} Sent!"
+    expect(page).to have_content "Friend Request to #{@test_user_two.full_name} Sent!"
 
     click_link "Sign Out"
 
-    sign_in_as(test_user_two)
+    sign_in_as(@test_user_two)
 
     expect(page).to have_content "New Friend Request!"
 
@@ -38,33 +40,41 @@ feature "User adds friends", %{
 
     expect(page).to have_content "Friendship Accepted"
 
-    click_link test_user_one.full_name
-    expect(current_path) == user_path(test_user_one)
+    click_link @test_user_one.full_name
+    expect(current_path) == user_path(@test_user_one)
   end
 
   scenario "user rejects friend request" do
-    test_user_one = FactoryGirl.create(:user)
-    test_user_one.reindex
-    User.searchkick_index.refresh
-    test_user_two = FactoryGirl.create(:user, first_name: "Michael")
-    test_user_two.reindex
-    User.searchkick_index.refresh
+    sign_in_as(@test_user_one)
 
-    sign_in_as(test_user_one)
-
-    visit user_path(test_user_two)
+    visit user_path(@test_user_two)
 
     click_link "Add Friend"
 
     click_link "Sign Out"
 
-    sign_in_as(test_user_two)
+    sign_in_as(@test_user_two)
 
     click_link "Ignore"
 
-    expect(page).to have_content "Friend Request From #{test_user_one.full_name} Ignored"
+    expect(page).to have_content "Friend Request From #{@test_user_one.full_name} Ignored"
     expect(page).to_not have_content "New Friend Request!"
-    visit user_path(test_user_two)
-    expect(page).to_not have_content test_user_one.full_name
+    visit user_path(@test_user_two)
+    expect(page).to_not have_content @test_user_one.full_name
+  end
+  scenario "two users try to friend each other" do
+    sign_in_as(@test_user_one)
+
+    visit user_path(@test_user_two)
+
+    click_link "Add Friend"
+
+    click_link "Sign Out"
+
+    sign_in_as(@test_user_two)
+
+    visit user_path(@test_user_two)
+
+    expect(page).to_not have_content "Add Friend"
   end
 end
