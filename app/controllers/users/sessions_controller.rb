e0002a01dc56b027aeaf09899d6749fb596c3252
nickcluc <nclucas01@gmail.com>
2015-03-20 15:04:49 -0400

    styling

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -16,10 +16,13 @@ class Users::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
 
   # You can put the params you want to permit in the empty array.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.for(:sign_in) << :attribute
   # end
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
 end
