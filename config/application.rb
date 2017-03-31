a0725cb785b3a6221403937edd7ef659c852ad83
Nick Clucas <nick.clucas@drugdev.com>
2017-03-31 11:14:17 -0400

    test

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
-
+# unused comment
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
